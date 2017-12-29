272050b92ba5f1770f4165402837b8d3612c92a9
yangjunbao <yangjunbao@oneapm.com>
2017-12-29 14:49:54 +0800

    threesum   all nums

--- src/com/apptao/leetcode/APlusB.java
+++ src/com/apptao/leetcode/APlusB.java
@@ -1,7 +1,6 @@
 package com.apptao.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by a on 2017-12-25.
@@ -9,7 +8,7 @@ import java.util.Map;
 public class APlusB {
 
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("aplusb" + "----------------------------------------------");
         int aplusb = aplusb(3, 5);
         System.out.println(aplusb + "");
@@ -19,16 +18,25 @@ public class APlusB {
         for (int i = 0; i < twoSum.length; i++) {
             System.out.println(twoSum[i]);
         }
+
+
+        int[] nums = {-1, 0, 1, 2, -1, -4, -1, 0, 1, 2, -1, -4, -1, 0, 1, 2, -1, -4, -1, 0, 1, 2, -1, -4, -1, 0, 1, 2, -1, -4, -1, 0, 1, 2, -1, -4, -1, 0, 1, 2, -1, -4, -1, 0, 1, 2, -1, -4};
+
+        List<List<Integer>> lists = threeSum(nums, 2);
+        System.out.println(lists.toString());
+
     }
 
-    /** aplusb 不用运算符+ commit
+    /**
+     * aplusb 不用运算符+ commit
+     *
      * @param a
      * @param b
      * @return
      */
     private static int aplusb(int a, int b) {
 
-        while (b!=0) {
+        while (b != 0) {
             int tempA = a ^ b;
             int tempB = (a & b) << 1;
             a = tempA;
@@ -41,6 +49,7 @@ public class APlusB {
 
     /**
      * 给定数组，给定目标，计算数组中有没有相加的和为目标值；
+     *
      * @param nums
      * @param target
      * @return
@@ -48,7 +57,7 @@ public class APlusB {
     public static int[] twoSum(int[] nums, int target) {
         //map 存放 运算值
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i<nums.length; i ++) {
+        for (int i = 0; i < nums.length; i++) {
             //反向计算获取 数组中的值
             int complement = target - nums[i];
 
@@ -64,6 +73,56 @@ public class APlusB {
 
 
     }
+
+
+    /**
+     *
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static List<List<Integer>> threeSum(int [] nums, int target) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 3) return result;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] != nums[i -1]) {
+
+                int j = i +1, k = nums.length - 1;
+                while (k > j) {
+                    if (j != i + 1 && nums[j] == nums[j-1]) {
+                        j++;
+                        continue;
+                    }
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        k--;
+                        j++;
+                    } else if (sum > target) {
+                        k--;
+                    } else {
+                        j++;
+                    }
+                }
+
+
+            }
+
+
+
+
+        }
+
+        return result;
+
+
+
+    }
+
 
 
 }
